82cba75bfd5e07b3401284164ac0f69afbe3779c
Rishab Chadha <chadha.rishab@gmail.com>
2016-06-25 21:53:35 -0400

    refactored login. Added login class

---
+++ AuthService.js
@@ -0,0 +1,40 @@
+var buffer = require('buffer');
+
+class AuthService {
+	login(creds, cb){
+		var b = new buffer.Buffer(creds.username + 
+				':' + creds.password);
+			var encodedAuth = b.toString('base64');
+			//console.log(b.toString('base64'));
+
+			fetch('https://api.github.com/user',{
+				headers: {
+					'Authorization' : 'Basic ' + encodedAuth
+				}
+			})
+			.then((response)=> {
+				if(response.status >= 200 && response.status < 300){
+					return response;
+				}
+
+				throw {
+					badCredentials: response.status == 401,
+					unknownError: response.status != 401
+				}
+			})
+			.then((response)=> {
+				return response.json();
+			})
+			.then((results)=> {
+				console.log(results);
+				return cb({success: true});
+				//this.setState({showProgress: false});
+			})
+			.catch((err)=> {
+				//console.log('logon failed:' + err);
+				return cb(err);
+			});
+	}
+}
+
+module.exports = new AuthService();
